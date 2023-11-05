59f05c5b21f478ed7e1309c280a5f9f986b1e2aa
NatividadJouard <145673693+NatividadJouard@users.noreply.github.com>
2023-11-05 17:27:21 +0800

    fixation of logical error StudentMethods.java

--- StudentMethods.java
+++ StudentMethods.java
@@ -14,77 +14,90 @@ import java.util.Scanner;
     
         public static void AddSRecord(Scanner scan, Student[] students) {
         
-        for (int i = 0; i < students.length; i++) {
-            
-            if (students[i] == null) {
-                System.out.println("Enter student name:");
-                String name = scan.nextLine();
-                System.out.println("Enter student course:");
-                String course = scan.nextLine();
-                double gwa = 0;
-                
-                boolean numGwa = false;
-                while (!numGwa) {
-                    System.out.println("Enter student GWA:");
-                    if (scan.hasNextDouble()) {
-                        gwa = scan.nextDouble();
-                        numGwa = true;
-                    } 
-                    else {
-                        System.out.println("Only numbers are allowed for GWA. Please try again.");
-                        scan.next(); 
-                    }
-                }
-                    students[i] = new Student(name, course, gwa);
-                    scan.nextLine(); 
-                    break;
-            }
-                    if (i == students.length - 1) {
-                    System.out.println("Student record limit reached.");
-        }
-        }
-        }
+            for (int i = 0; i < students.length; i++) {
 
-        public static void EditSRecord(Scanner scan, Student[] students) {
-            System.out.println("Enter the number of the student you want to edit from (1 to " + students.length + "):");
+                if (students[i] == null) {
+                    System.out.println("Enter student name:");
+                    String name = scan.nextLine();
+                    System.out.println("Enter student course:");
+                    String course = scan.nextLine();
+                    double gwa = 0;
 
-            while (!scan.hasNextInt()) {
-                System.out.println("Invalid Key. Please enter a valid number.");
-                scan.next();
-            }
-                int SI = scan.nextInt() - 1;
-                scan.nextLine(); 
-
-                if (SI >= 0 && SI < students.length && students[SI] != null) {
-                    System.out.println("Enter new name:");
-                    String newName = scan.nextLine();
-                    System.out.println("Enter new course:");
-                    String newCourse = scan.nextLine();
-                    double newGwa = 0;
                     boolean numGwa = false;
-
                     while (!numGwa) {
-                        System.out.println("Enter new GWA:");
-
+                        System.out.println("Enter student GWA:");
                         if (scan.hasNextDouble()) {
-                            newGwa = scan.nextDouble();
+                            gwa = scan.nextDouble();
                             numGwa = true;
                         } 
                         else {
                             System.out.println("Only numbers are allowed for GWA. Please try again.");
-                            scan.next();
+                            scan.next(); 
                         }
                     }
-                            students[SI].setName(newName);
-                            students[SI].setCourse(newCourse);
-                            students[SI].setGwa(newGwa);
-                            System.out.println("Student information updated.");
-                            scan.nextLine();
-                }   
-                        else {
-                            System.out.println("No Student Found.");
+                        students[i] = new Student(name, course, gwa);
+                        scan.nextLine(); 
+                        break;
+                }       
+                        if (i == students.length - 1) {
+                        System.out.println("Student record limit reached.");
                         }
+            }
         }
+
+        public static void EditSRecord(Scanner scan, Student[] students) {
+            boolean viewStudents = false;
+            for (int i = 0; i < students.length; i++) {
+                if (students[i] != null) {
+                viewStudents = true;
+                ViewSRecords(students);
+                System.out.println("Student " + (i + 1) + ":");
+                System.out.println("Name: " + students[i].getName() + "\nCourse: " + students[i].getCourse() + "\nGWA: " + students[i].getGwa());
+                }
+            }
+            if (!viewStudents) {
+                System.out.println("No Student Found.");
+                return;
+            }
+                System.out.println("Enter the number of the student you want to edit from (1 to " + students.length + "):");
+                while (!scan.hasNextInt()) {
+                    System.out.println("Invalid Key. Please enter a valid number.");
+                    scan.next();
+                }
+            int SI = scan.nextInt() - 1;
+            scan.nextLine();
+
+            if (SI >= 0 && SI < students.length && students[SI] != null) {
+                System.out.println("Enter new name:");
+                String newName = scan.nextLine();
+                System.out.println("Enter new course:");
+                String newCourse = scan.nextLine();
+                double newGwa = 0;
+                boolean numGwa = false;
+
+                while (!numGwa) {
+                System.out.println("Enter new GWA:");
+
+                    if (scan.hasNextDouble()) {
+                        newGwa = scan.nextDouble();
+                        numGwa = true;
+                    } 
+                    else {
+                    System.out.println("Only numbers are allowed for GWA. Please try again.");
+                    scan.next();
+                    }
+            }
+            students[SI].setName(newName);
+            students[SI].setCourse(newCourse);
+            students[SI].setGwa(newGwa);
+            System.out.println("Student information updated.");
+            scan.nextLine();
+            } 
+            else {
+            System.out.println("No Student Found.");
+            }
+}
+
 
         public static void ViewSRecords(Student[] students) {
             
@@ -94,7 +107,6 @@ import java.util.Scanner;
                 ViewStudents = true;
                 System.out.println("Student " + (i + 1) + ":");
                 System.out.println("Name: " + students[i].getName() + "\nCourse: " + students[i].getCourse() + "\nGWA: " + students[i].getGwa());
-//              System.out.println("_______________________________");
                 System.out.println("");  
                 }
             }
@@ -103,40 +115,34 @@ import java.util.Scanner;
                 }
         }
 
-        public static void SearchSRecord(Scanner scan, Student[] students) {
-            System.out.println("Enter the number of the student you want to search from (1 to " + students.length + "):");
+        public static void DeleteSRecord(Scanner scan, Student[] students) {
+            
+            boolean ViewStudents = false;
+            for (int i = 0; i < students.length; i++) {
+                if (students[i] != null) {
+                ViewStudents = true;
+                ViewSRecords(students);
+                System.out.println("Enter the number of the student you want to delete from (1 to " + students.length + "):");
+                    while (!scan.hasNextInt()) {
+                    System.out.println("Invalid Key. Please enter a valid number.");
+                    scan.next();
+                    }
+                    int SI = scan.nextInt() - 1;
+                    scan.nextLine(); 
 
-            while (!scan.hasNextInt()) {
-                System.out.println("Invalid Key. Please enter a valid number.");
-                scan.next();
+                    if (SI >= 0 && SI < students.length && students[SI] != null) {
+                        students[SI] = null;
+                        System.out.println("Selected Student is deleted.");
+                    } 
+                    else {
+                        System.out.println("No Student Found.");
+                    }
+                
+                }
             }
-                int SI = scan.nextInt() - 1;
-                scan.nextLine();
-                if (SI >= 0 && SI < students.length && students[SI] != null) {
-                    Student student = students[SI];
-                    System.out.println("Student found:");
-                    System.out.println("Name: " + student.getName() + "\nCourse: " + student.getCourse() + "\nGWA: " + student.getGwa());
-                } 
-                else {
+                if (!ViewStudents) {
                     System.out.println("No Student Found.");
                 }
+                
         }
-
-        public static void DeleteSRecord(Scanner scan, Student[] students) {                            
-            System.out.println("Enter the number of the student you want to delete from (1 to " + students.length + "):");
-
-            while (!scan.hasNextInt()) {
-                System.out.println("Invalid Key. Please enter a valid number.");
-                scan.next();
-            }
-                int SI = scan.nextInt() - 1;
-                scan.nextLine(); 
-
-                if (SI >= 0 && SI < students.length && students[SI] != null) {
-                    students[SI] = null;
-                    System.out.println("Selected Student is deleted.");
-                } else {
-                    System.out.println("No Student Found.");
-                }
-        }
-}
+    }
