e861630c34b1c2170dd4be6f57ea76aa63a3ce63
NatividadJouard <145673693+NatividadJouard@users.noreply.github.com>
2024-01-16 21:21:38 +0800

    Add files via upload

---
+++ Quadrilateral.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package runquad;
+
+/**
+ *
+ * @author eduna
+ */
+public class Quadrilateral {
+    public void showDescription() {
+        System.out.println("- is a quadrilateral");
+    }
+}
+
+    class Parallelogram extends Quadrilateral {
+        @Override
+        public void showDescription() {
+            super.showDescription();
+            System.out.println("- has 2 pairs of parallel sides");
+        }
+    }
+
+        class Rectangle extends Parallelogram {
+            @Override
+            public void showDescription() {
+                super.showDescription();
+                System.out.println("- has 4 right angles");
+            }
+        }
+
+            class Square extends Rectangle {
+                @Override
+                public void showDescription() {
+                    super.showDescription();
+                    System.out.println("- has 4 equal sides");
+                }
+            }
+
+                class Rhombus extends Parallelogram {
+                    @Override
+                    public void showDescription() {
+                        super.showDescription();
+                        System.out.println("- has 4 congruent sides");
+                    }
+                }
+
+                    class Trapezoid extends Quadrilateral {
+                        @Override
+                        public void showDescription() {
+                            super.showDescription();
+                            System.out.println("- has 1 pair of parallel sides");
+                        }
+                    }
---
+++ RunQuad.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package runquad;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author eduna
+ */
+public class RunQuad {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Press R for Rectangle, S for Square, P for Parallelogram, H for Rhombus, T for Trapezoid, or E to exit: ");
+            char choice = scanner.next().charAt(0);
+
+            if (choice == 'E' || choice == 'e') {
+                System.out.println("Shapes are Fun.");
+                break;
+            }
+
+            switch (choice) {
+                case 'R':
+                case 'r':
+                    System.out.println("Rectangle: ");
+                    Rectangle rectangle = new Rectangle();
+                    rectangle.showDescription();
+                    break;
+                case 'S':
+                case 's':
+                    System.out.println("Square: ");
+                    Square square = new Square();
+                    square.showDescription();
+                    break;
+                case 'P':
+                case 'p':
+                    System.out.println("Parallelogram: ");
+                    Parallelogram parallelogram = new Parallelogram();
+                    parallelogram.showDescription();
+                    break;
+                case 'H':
+                case 'h':
+                    System.out.println("Rhombus: ");
+                    Rhombus rhombus = new Rhombus();
+                    rhombus.showDescription();
+                    break;
+                case 'T':
+                case 't':
+                    System.out.println("Trapezoid: ");
+                    Trapezoid trapezoid = new Trapezoid();
+                    trapezoid.showDescription();
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please press R, S, P, H, T, or E.");
+            }
+        }
+
+        scanner.close();
+    }
+}
